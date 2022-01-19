89a1abed95e345f00cf37443f871afcc533d51d1
ruwan9 <ruwan9@kakao.com>
2022-01-19 22:08:24 +0900

    feat: useMemo added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -47,10 +47,25 @@ function App() {
   const onEdit = (targetId, newContent) => {
     setData(data.map((it) => (it.id === targetId ? { ...it, content: newContent } : it)));
   };
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
 
   return (
     <div>
       <DiaryEditor onCreate={onCreate} />
+      <div>
+        <div>전체 일기: {data.length}</div>
+        <div>기분이 좋은 일기: {goodCount}</div>
+        <div>기분이 안좋은 일기: {badCount}</div>
+        <div>기분 좋은 일기 비율: {goodRatio} %</div>
+      </div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} itemList={data} />
     </div>
   );
